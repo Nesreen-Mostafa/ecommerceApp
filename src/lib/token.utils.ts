2d1b67124b9b40dfa3062091edf57bd7d0c32b91
Nesreen-Mostafa <nesreenmos19@gmail.com>
2025-09-20 22:31:20 +0300

    updates

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 export async function getUserToken() {
 
-    const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
+    const encodedToken = (await cookies()).get("next-auth.session-token")?.value || (await cookies()).get("__Secure-next-auth.session-token")?.value ;
     const decrepToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! })
     const token = decrepToken?.token
     
